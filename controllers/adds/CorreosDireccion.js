ee1e29cf3b2512c4cd8d87394c538da4d6a3fa8c
DrackZero <johanmarjas@gmail.com>
2024-06-06 19:17:07 -0500

    Update CorreosDireccion.js

--- controllers/adds/CorreosDireccion.js
+++ controllers/adds/CorreosDireccion.js
@@ -4,15 +4,12 @@ document.addEventListener('DOMContentLoaded', async () => {
   try {
     console.log('El evento DOMContentLoaded se ha activado');
     
-    // Obtiene las direcciones de correo electrónico de los vehículos registrados
     const direccionesCorreo = await obtenerDireccionesCorreo();
     console.log('Direcciones de correo electrónico obtenidas:', direccionesCorreo);
 
-    // Obtiene el select del formulario
     const selectElement = document.getElementById('to');
     console.log('Elemento select encontrado:', selectElement);
 
-    // Itera sobre las direcciones de correo electrónico y crea opciones para el select
     direccionesCorreo.forEach((direccion) => {
       const option = document.createElement('option');
       option.value = direccion;
@@ -24,8 +21,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 });
 
-// Agrega el código para enviar correos aquí
-import { enviarCorreo } from '../adds/email.js'; // Suponiendo que esta función está definida en CorreosDireccion.js
+import { enviarCorreo } from '../adds/email.js'; 
 const form = document.getElementById('emailForm');
 console.log('Formulario encontrado:', form);
 
