10683de96dc8278d0707483a5393595177790592
DrackZero <johanmarjas@gmail.com>
2024-10-18 01:22:44 -0500

    Update AgregarV.js

--- controllers/Register/AgregarV.js
+++ controllers/Register/AgregarV.js
@@ -1,6 +1,5 @@
 import { cargarArchivo, AgregarVehiculo } from '../Conection/functions.js';
 
-const guardar = document.getElementById('btnsave');
 
 async function guardarVehiculo(event) {
     event.preventDefault();
@@ -16,15 +15,15 @@ async function guardarVehiculo(event) {
     const capacidad = parseInt(document.getElementById('capacidad').value);
     const estado = document.getElementById('estado').value;
     const soatFile = document.getElementById('Soat').files[0];
-    const fechaSoat = new Date(document.getElementById('fechaS').value);
+    const fechaSoat = new Date(document.getElementById('fechaS').value + 'T00:00:00-05:00');
     const email = document.getElementById('email').value;
 
     const revisionTecFile = document.getElementById('revisionTec').files[0];
     const tarjetaPropiedadFile = document.getElementById('tarjetaPropiedad').files[0];
     const tarjetaOperacionFile = document.getElementById('tarjetaOperacion').files[0];
-    const fechaRevisionTec = new Date(document.getElementById('fechaRT').value);
-    const fechaTarjetaOperacion = new Date(document.getElementById('fechaT').value);
-    const fechaTarjetaPropiedad = new Date(document.getElementById('fechaTP').value);
+    const fechaRevisionTec = new Date(document.getElementById('fechaRT').value + 'T00:00:00-05:00');
+    const fechaTarjetaOperacion = new Date(document.getElementById('fechaT').value + 'T00:00:00-05:00');
+    const fechaTarjetaPropiedad = new Date(document.getElementById('fechaTP').value + 'T00:00:00-05:00');
 
 
     if (!fechaRevisionTec || !fechaTarjetaOperacion) {
